bb2638ad13f5d94d0d443909ec278eb006bdb1e4
Guillaume Valvona <valvone96@gmail.com>
2019-06-14 15:07:57 +0200

    Replaced inline definitions

--- Movie.h
+++ Movie.h
@@ -3,6 +3,7 @@
 // Movie.h
 #ifndef MOVIE_H
 #define MOVIE_H
+
 #include <string>
 
 class Movie {
@@ -11,36 +12,24 @@ public:
     static const int REGULAR     = 0;
     static const int NEW_RELEASE = 1;
 
-    explicit Movie( std::string  title, int priceCode = REGULAR );
+    explicit Movie( std::string  title, int priceCode = REGULAR ): _title(std::move( title )), _priceCode( priceCode )
+    {}
 
-    int getPriceCode() const;
-    void setPriceCode( int arg );
-    std::string getTitle() const;
+    int getPriceCode() const {
+        return _priceCode;
+    }
+
+    void setPriceCode( int arg ) {
+        _priceCode = arg;
+    }
+
+    std::string getTitle() const {
+        return _title;
+    }
 
 private:
     std::string _title;
     int _priceCode;
 };
-
-inline Movie::
-Movie( std::string  title, int priceCode )
-        : _title(std::move( title ))
-        , _priceCode( priceCode )
-{}
-
-inline int Movie::
-getPriceCode() const {
-    return _priceCode;
-}
-
-inline void Movie::
-setPriceCode( int arg ) {
-    _priceCode = arg;
-}
-
-inline std::string Movie::
-getTitle() const {
-    return _title;
-}
 
 #endif // MOVIE_H
