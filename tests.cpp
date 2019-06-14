4d5dc5bf6776bc71d4bf4d04d968f0d8797c60e2
Guillaume Valvona <valvone96@gmail.com>
2019-06-14 14:42:55 +0200

    Test of output working

--- tests.cpp
+++ tests.cpp
@@ -12,8 +12,8 @@ TEST(statementTests, statementTest1) {
     customer.addRental( Rental( Movie("Karate Kid"), 7));
     customer.addRental( Rental( Movie( "Avengers: Endgame", Movie::NEW_RELEASE ), 5));
     customer.addRental( Rental( Movie("Snow White", Movie::CHILDRENS), 3 ));
-
-    std::cout << customer.statement() << std::endl;
+    std::string result = "Rental Record for Olivier\n\tKarate Kid\t9.5\n\tAvengers: Endgame\t15\n\tSnow White\t1.5\nAmount owed is 26\nYou earned 4 frequent renter points";
+    ASSERT_EQ(result, customer.statement());
 }
 
 int main(int argc, char **argv) {
